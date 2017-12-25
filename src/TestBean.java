56220dd36d7301eb5fab80614a0bd16e25649aa8
fawad1997 <30860709+fawad1997@users.noreply.github.com>
2017-12-25 23:01:12 +0500

    PrimeFaces Conversion Started

--- src/TestBean.java
+++ src/TestBean.java
@@ -1,7 +1,29 @@
 import javax.faces.bean.ManagedBean;
-@ManagedBean
-public class TestBean {
-    TestBean(){
+import java.util.ArrayList;
+import java.util.List;
 
+@ManagedBean(name = "test")
+public class TestBean {
+    List<String> selectedSkills;
+    public TestBean(){
+        selectedSkills = new ArrayList<>();
+    }
+
+    public List<String> getSelectedSkills() {
+        return selectedSkills;
+    }
+
+    public void setSelectedSkills(List<String> selectedSkills) {
+        this.selectedSkills = selectedSkills;
+    }
+
+    public List<String> completeTheme(String query) {
+        List<String> filteredThemes = new ArrayList<String>();
+
+        for (int i = 0; i < 5; i++) {
+             filteredThemes.add("Fawad"+i);
+        }
+
+        return filteredThemes;
     }
 }
