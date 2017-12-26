a9c3a02bed52975bfdc80ddce94d49a36e42b630
fawad1997 <30860709+fawad1997@users.noreply.github.com>
2017-12-26 23:54:42 +0500

    Session Management Started

--- src/TestBean.java
+++ src/TestBean.java
@@ -1,4 +1,6 @@
 import TestBackingBeans.User;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
@@ -6,6 +8,7 @@ import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,10 +16,12 @@ import java.util.List;
 public class TestBean {
     //Fields
     private User user = new User();
+
     //Constructors
     public TestBean() {
     }
     //Getter and Setters
+
     public User getUser() {
         return user;
     }
@@ -32,6 +37,7 @@ public class TestBean {
             System.out.println("Not Null");
             FacesContext context = FacesContext.getCurrentInstance();
             context.getExternalContext().getSessionMap().put("email",user.getEmail());
+            context.getExternalContext().getSessionMap().put("jobseeker",true);
             return "welcome.xhtml?faces-redirect=true";
         }else {
             System.out.println("Else Part");
